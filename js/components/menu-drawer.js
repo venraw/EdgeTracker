eda8be9e3f1a18c512a88ef1ae5f20f672eddc23
Vincent <venraw225@gmail.com>
2026-09-16 19:27:04 +0100

    added overlay on hambuger and menu drawer

--- js/components/menu-drawer.js
+++ js/components/menu-drawer.js
@@ -1,7 +1,31 @@
+// Selectors
 const createBtn = document.querySelector('.create-btn');
-const menuDrawer = document.querySelector('.menu-drawer')
-const overlay = document.querySelector('.drawer-overlay');
+const menuDrawer = document.querySelector('.menu-drawer');
+const drawerOverlay = document.querySelector('.drawer-overlay');
 
-createBtn.addEventListener('click', () => {
-    menuDrawer.classList.add('open');
-})
+const hamburgerBtn = document.querySelector('.hamburger-menu');
+const hamburgerMenu = document.querySelector('.hamburger-menu-list');
+const menuOverlay = document.querySelector('.menu-overlay');
+//const createOverlay = document.getElementById('drawerOverlay');
+
+// Toggle Hamburger Menu
+function toggleHamburger() {
+  if (hamburgerMenu) hamburgerMenu.classList.toggle('open');
+  if (menuOverlay) menuOverlay.classList.toggle('open');
+  document.body.classList.toggle('no-scroll');
+}
+
+// Toggle Create Drawer
+function toggleDrawer() {
+  if (menuDrawer) menuDrawer.classList.toggle('open');
+  if (drawerOverlay) drawerOverlay.classList.toggle('open');
+  document.body.classList.toggle('no-scroll');
+}
+
+
+if (hamburgerBtn) hamburgerBtn.addEventListener('click', toggleHamburger);
+if (menuOverlay) menuOverlay.addEventListener('click', toggleHamburger);
+
+
+if (createBtn) createBtn.addEventListener('click', toggleDrawer);
+if (drawerOverlay) drawerOverlay.addEventListener('click', toggleDrawer);
